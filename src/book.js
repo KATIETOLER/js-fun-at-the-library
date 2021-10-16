16267d04a2d77c95139b0e907490e37c5e7d9654
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2021-10-16 14:26:52 -0600

    Add book.js 13 passing) ls kk clear clear()

--- src/book.js
+++ src/book.js
@@ -3,11 +3,48 @@ function createTitle(bookIdea) {
   return 'The ' + bookIdea
 };
 
+function buildMainCharacter(a,b,c)
+{
+  var character =
+    {
+    name: a,
+    age: b,
+    pronouns: c,
+  }
+  return character
+};
+
+function saveReview(addString,reviews) {
+    if (!reviews.includes(addString)) {
+      reviews.push(addString)
+  }
+  return reviews;
+};
+
+function calculatePageCount(createTitle) {
+return createTitle.toString().length*20
+};
+
+
+function writeBook(a,b,c) {
+  var book = [
+    {
+    title: createTitle(),
+    mainCharacter: buildMainCharacter(),
+    pageCount: calculatePageCount(),
+    genre: 'fantasy',
+    }
+  ]
+  return book
+};
+
+
+
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
   // editBook
 }
