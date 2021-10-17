415c4861a5063d8baa208136df1e7ab9771af872
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2021-10-17 17:56:31 -0600

    Add some passing librarians

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,19 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    //constructor creates a mass number of object through typing 'new Librarian'!
+    //inside constructors is (almost?)always an object function specifically for classes which are themselves for objects
+    //constructor objects use semicolons!!! don't forget!!!
+    this.name = name;
+    this.library = library;
+    }
+
+  greetPatron(name, isMorning) {
+    if (isMorning===true) {
+    return `Good morning, ${name}!`
+    };
+    return `Hello, ${name}!`
+
   }
-}
+};
 
 module.exports = Librarian;
