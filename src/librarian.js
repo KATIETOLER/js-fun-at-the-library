7e26f8e90baae27f51ca95569704374b26d495b1
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2021-10-17 18:42:44 -0600

    Add librarian and tests

--- src/librarian.js
+++ src/librarian.js
@@ -5,15 +5,39 @@ class Librarian {
     //constructor objects use semicolons!!! don't forget!!!
     this.name = name;
     this.library = library;
-    }
+  }
 
   greetPatron(name, isMorning) {
-    if (isMorning===true) {
+    if (isMorning) {
     return `Good morning, ${name}!`
     };
     return `Hello, ${name}!`
-
   }
+
+findBook(bookName) {
+  var genres = [].concat(
+    this.library.shelves.fantasy,
+    this.library.shelves.fiction,
+    this.library.shelves.nonFiction)
+
+for (var i = 0; i < genres.length; i++) {
+    if (bookName === genres[i].title) {
+      this.library.shelves[genres[i].genre].splice(i, 1)
+      //the genre is a string which is called through bracket notation is the equivalent of the name of the variable shelf
+      //dynamic value of loose comparison
+      return `Yes, we have ${bookName}`
+    }
+      }
+    return `Sorry, we do not have ${bookName}`
+  }
+
+calculateLateFee(days) {
+  return Math.ceil(days*.25)
+}
+
 };
+
+
+
 
 module.exports = Librarian;
